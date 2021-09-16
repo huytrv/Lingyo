59e55245ffc9adb842902143094c89e320b3e872
Trần Huy <38495100+huytrv@users.noreply.github.com>
2021-09-16 10:59:35 +0700

    Update home_dev.js

--- lingyo/public/js/home_dev.js
+++ lingyo/public/js/home_dev.js
@@ -5660,9 +5660,9 @@ function handleUpdateProfile(){
                         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
                         const resizedDetections = faceapi.resizeResults(detections, displaySize)
                         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-                        faceapi.draw.drawDetections(canvas, resizedDetections)
-                        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-                        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+                        // faceapi.draw.drawDetections(canvas, resizedDetections)
+                        // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+                        // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
                         if (detections[0]){
                             if (!detections[0].expressions){faceValid = false}
                             else {
