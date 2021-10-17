7380d4499b6670a1b0abfe3efd83eb6fefcea7eb
Trần Huy <38495100+huytrv@users.noreply.github.com>
2021-10-17 15:06:16 +0700

    17/10

--- lingyo/public/js/login_dev.js
+++ lingyo/public/js/login_dev.js
@@ -15,7 +15,7 @@ if (window.location.pathname == "/login") {
             history.pushState({
                 id: 'login'
             }, '', 'https://lingyo.vn/login')
-            document.querySelector('title').textContent = "Lingyo - Mạng xã hội bình chọn"
+            document.querySelector('title').textContent = "Lingyo - Vòng đấu nhảy múa trực tuyến"
         }
     }
     xhttpLogin.open("GET", "/login", true)
@@ -97,7 +97,7 @@ const signupHandle = function(){
     history.pushState({
         id: 'signup'
     }, '', 'https://lingyo.vn/signup')
-    document.querySelector('title').textContent = "Lingyo - Mạng xã hội bình chọn"
+    document.querySelector('title').textContent = "Lingyo - Vòng đấu nhảy múa trực tuyến"
     if (document.querySelectorAll(".login-redirect").length != 0){
         document.querySelector(".login-redirect").onclick = function(){
             document.querySelector(".modal-content").classList.add("modal-remove-down")
@@ -106,8 +106,8 @@ const signupHandle = function(){
             }
             history.pushState({
                 id: 'login'
-            }, 'Lingyo - Mạng xã hội bình chọn', 'https://lingyo.vn/login')
-            document.querySelector('title').textContent = "Lingyo - Mạng xã hội bình chọn"
+            }, 'Lingyo - Vòng đấu nhảy múa trực tuyến', 'https://lingyo.vn/login')
+            document.querySelector('title').textContent = "Lingyo - Vòng đấu nhảy múa trực tuyến"
         }
     }
 
