197ce407b5ead126a7ce73641e7d6153dde48ae9
Trần Huy <38495100+huytrv@users.noreply.github.com>
2021-11-19 08:32:23 +0700

    19/11

--- lingyo/index.js
+++ lingyo/index.js
@@ -472,7 +472,7 @@ app.use(session({
     cookie: {
         secure: true,
         maxAge: 1000*60*60*24*365,
-        sameSite: 'none'
+        // sameSite: 'none'
     },
     store: sessionStore,
     httpOnly: true,
